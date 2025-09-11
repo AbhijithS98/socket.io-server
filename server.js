55e7c0f0ca3f00f403ccf017c0abcc1de2598b01
AbhijithS98 <abhijithsb745@gmail.com>
2025-09-11 19:55:05 +0530

    dotenv loaded correctly

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import http from "http";
 import { createClient } from "redis";
 import { Server } from "socket.io";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 // Redis setup
 const redis = createClient();
