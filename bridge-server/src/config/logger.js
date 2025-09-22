7380d1ec5a05b98bb6912a503ac3dfdf01c192db
AbhijithS98 <abhijithsb745@gmail.com>
2025-09-22 20:09:20 +0530

    ESM syntax fix

--- bridge-server/src/config/logger.js
+++ bridge-server/src/config/logger.js
@@ -1,7 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { createLogger, format, transports } from 'winston';
+
 const { combine, timestamp, printf, colorize } = format;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const logDir = path.join(__dirname, '../logs');
 
