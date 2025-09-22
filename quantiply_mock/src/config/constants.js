c2de693a432270b775e16f407a135890f34a4520
AbhijithS98 <abhijithsb745@gmail.com>
2025-09-22 20:37:01 +0530

    instanceID of qpy-mock server changed to constant

--- quantiply_mock/src/config/constants.js
+++ quantiply_mock/src/config/constants.js
@@ -1,4 +1,4 @@
-export const INSTANCE_ID = `QPY-${process.pid}`;
+export const INSTANCE_ID = `QPY-server-1`;
 export const JOBS_STREAM = "jobsStream";
 export const RESPONSES_STREAM = "responsesStream";
 export const RESPONSES_GROUP = `responsesGroup-${INSTANCE_ID}`;
